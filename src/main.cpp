5558c434c412eb652b1d37a2b768a21af8cfc290
bjonke <r.skeppstrom@gmail.com>
2022-03-23 03:42:00 +0100

    unitest doctest trial

--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,8 @@
 #include <random>
 #include <sqlite3.h>
 #include "../rapidxml-1.13/rapidxml.hpp"
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "../include/doctest.h"
 
 using namespace std;
 using namespace rapidxml;
@@ -237,8 +239,53 @@ void updateHungerAndThirst()
         playerCharacter[playerCharacterSelected].hitpoints_current--;
 }
 
+TEST_CASE("updateHungerAndThirst") {
+    if( playerCharacter[playerCharacterSelected].hunger == 100 )
+    CHECK(playerCharacter[playerCharacterSelected].hitpoints_current == 99);
+}
+
+TEST_CASE("[math] basic stuff") {
+    CHECK(6 > 5);
+    CHECK(6 > 7);
+}
+TEST_CASE("[math] basic stuff") {
+    CHECK(6 > 5);
+    CHECK(6 > 7);
+}
+
 int main(int argc, char ** argv)
 {
+    doctest::Context context;
+
+    // !!! THIS IS JUST AN EXAMPLE SHOWING HOW DEFAULTS/OVERRIDES ARE SET !!!
+
+    // defaults
+    context.addFilter("test-case-exclude", "*math*"); // exclude test cases with "math" in the name
+    context.setOption("rand-seed", 324);              // if order-by is set to "rand" use this seed
+    context.setOption("order-by", "file");            // sort the test cases by file and line
+
+    context.applyCommandLine(argc, argv);
+
+    // overrides
+    context.setOption("no-breaks", true); // don't break in the debugger when assertions fail
+
+    int resu = context.run(); // run queries, or run tests unless --no-run is specified
+
+    if(context.shouldExit()) // important - query flags (and --exit) rely on the user doing this
+        return resu;          // propagate the result of the tests
+
+    context.clearFilters(); // removes all filters added up to this point
+
+    //int client_stuff_return_code = program();
+    //some_program_code(argc, argv);
+    // your program - if the testing framework is integrated in your production code
+
+    //return resu + client_stuff_return_code; // the result from doctest is propagated here as well
+
+    SDL_Delay(10000);
+
+
+
    cout << "\nParsing my students data (sample.xml)....." << endl;
 
     // Read the sample.xml file
