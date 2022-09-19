76db4931b0d67bc5d586f3c0164f8a9e204f9ab5
bjonke <r.skeppstrom@gmail.com>
2022-09-19 08:35:23 +0200

    Added code files
    https://app.clickup.com/t/3fegvnw

---
+++ include/gameengine/worldmapstate.cpp
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_net.h>
+#include "gameengine.h"
+#include "gamestate.h"
+#include "menustate.h"
+#include "worldmapstate.h"
+
+CWorldMapState CWorldMapState::m_WorldMapState;
+
+void CWorldMapState::Init()
+{
+	printf("CWorldMapState Init\n");
+}
+
+void CWorldMapState::Cleanup()
+{
+	printf("CWorldMapState Cleanup\n");
+}
+
+void CWorldMapState::Pause()
+{
+	printf("CWorldMapState Pause\n");
+}
+
+void CWorldMapState::Resume()
+{
+	printf("CWorldMapState Resume\n");
+}
+
+void CWorldMapState::HandleEvents(CGameEngine* game)
+{
+	SDL_Event event;
+
+	if (SDL_PollEvent(&event)) {
+		switch (event.type) {
+			case SDL_QUIT:
+				game->Quit();
+				break;
+
+			case SDL_KEYDOWN:
+				switch (event.key.keysym.sym) {
+					case SDLK_SPACE:
+						break;
+
+					case SDLK_ESCAPE:
+						game->Quit();
+						break;
+				}
+				break;
+		}
+	}
+}
+
+void CWorldMapState::Update(CGameEngine* game)
+{
+}
+
+void CWorldMapState::Draw(CGameEngine* game)
+{
+    SDL_SetRenderDrawColor( game->renderer, 255, 255, 255, 255 );
+
+    SDL_RenderClear(game->renderer);
+
+	SDL_Texture* texture = game->LoadTexture("./images/menus/mainmenu.jpg",255);
+    SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_BLEND);
+    SDL_SetTextureAlphaMod( texture, 255 );
+    SDL_RenderCopy(game->renderer, texture, NULL, NULL);
+    SDL_DestroyTexture(texture);
+}
---
+++ include/gameengine/worldmapstate.h
@@ -0,0 +1,33 @@
+#ifndef WORLDMAPSTATE_H
+#define WORLDMAPSTATE_H
+
+#include <SDL2/SDL.h>
+#include "gamestate.h"
+
+class CWorldMapState : public CGameState
+{
+public:
+	void Init();
+	void Cleanup();
+
+	void Pause();
+	void Resume();
+
+	void HandleEvents(CGameEngine* game);
+	void Update(CGameEngine* game);
+	void Draw(CGameEngine* game);
+
+	static CWorldMapState* Instance() {
+		return &m_WorldMapState;
+	}
+
+protected:
+	CWorldMapState() { }
+
+private:
+	static CWorldMapState m_WorldMapState;
+
+	SDL_Texture* MainMenuBackgroundTexture;
+};
+
+#endif
